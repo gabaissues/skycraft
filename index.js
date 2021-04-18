dfdca2b3d9cd6c28829dd3bc5c34c68d1e735236
gbrsrs <ogabrielskj@gmail.com>
2021-04-18 19:55:28 -0300

    Criando um pacote na NPM

---
+++ index.js
@@ -0,0 +1,14 @@
+const { searchPage, start } = require('./lib/api.js')
+
+/**
+ 
+ * @author Gabriel <ogabrielskJ@gmail.com>
+ * @version 0.0.1
+ * @url https://github.com/gbrsrs/skycraft
+
+*/
+
+module.exports = {
+    start: start,
+    searchPage: searchPage
+}
